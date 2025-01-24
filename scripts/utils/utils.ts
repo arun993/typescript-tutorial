a2a2ace48fc26de4d9b549d16c9a73ae4a61f072
0xArun <117015135+arun993@users.noreply.github.com>
2025-01-24 06:11:31 +0530

    Update utils.ts

--- scripts/utils/utils.ts
+++ scripts/utils/utils.ts
@@ -1,6 +1,7 @@
 import { LicenseTerms, StoryClient, StoryConfig } from '@story-protocol/core-sdk'
 import { http, zeroAddress } from 'viem'
 import { privateKeyToAccount, Address, Account } from 'viem/accounts'
+import { client, account } from './utils'
 import dotenv from 'dotenv'
 dotenv.config()
 
